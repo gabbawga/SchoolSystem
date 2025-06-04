ae5ff1b54c2430c1fc0e2e3a98755e811c87e916
gabbawga <gabrielferreiraa05@gmail.com>
2025-06-04 16:56:14 -0300

    cadastro de class

---
+++ SchoolManagementSystem/Admin/AddClass.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class AddClass : System.Web.UI.Page
+    {
+        CommonFn.Commonfnx fn = new CommonFn.Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("SELECT ROW_NUMBER() OVER (ORDER BY ClassId) AS [Sr.No], ClassId, ClassName FROM Class");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = fn.Fetch("Select * from Class where ClassName = '"+txtClass.Text.Trim()+"' ");
+                if(dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Class values('" + txtClass.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Succesffully !";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtClass.Text = string.Empty;
+                    GetClass();
+                }
+                else
+                {
+                    lblMsg.Text = "Class Already Exist !";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('"+ex.Message+"');</script>");
+            }
+        
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetClass();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetClass();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetClass();    
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string ClassName = (row.FindControl("txtClassEdit") as TextBox).Text;
+                fn.Query("Update Class set ClassName = ' "+ClassName+"' where ClassId = '"+cId+"' ");
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetClass();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from Class where ClassId = '" + cId + "'");
+                lblMsg.CssClass = "alert alert-danger";
+                lblMsg.Text = "Delete Succesffully !";
+                GetClass();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script> alert('" + ex.Message + "');</script>");
+            }
+        }
+    }
+}
